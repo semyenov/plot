e498b68c65d685d4ea299dc10b6bd2856c068bfa
Adonis Kakoulidis <aklp08@gmail.com>
2023-09-20 09:38:52 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { writeFileSync } from 'node:fs'
+import { writeFile } from 'node:fs/promises'
 import process from 'node:process'
 
 import { JSDOM } from 'jsdom'
@@ -13,105 +13,102 @@ export interface IRow {
   value: number
 }
 
-// create virtual document
-const document = new JSDOM(`<!DOCTYPE html>
-<head><meta charset="UTF-8"></head>
-<body></body>`).window.document
+(async () => {
+  // create virtual document
+  const document = new JSDOM(`<!DOCTYPE html>
+  <head><meta charset="UTF-8"></head>
+  <body></body>`).window.document
 
-// // create client and query data
-// const client = createClient({
-//   host: process.env.CLICKHOUSE_HOST || 'localhost',
-//   username: process.env.CLICKHOUSE_USER || 'default',
-//   password: process.env.CLICKHOUSE_PASSWORD || 'default',
-//   database: process.env.CLICKHOUSE_DATABASE || 'default',
-// })
+  // // create client and query data
+  // const client = createClient({
+  //   host: process.env.CLICKHOUSE_HOST || 'localhost',
+  //   username: process.env.CLICKHOUSE_USER || 'default',
+  //   password: process.env.CLICKHOUSE_PASSWORD || 'default',
+  //   database: process.env.CLICKHOUSE_DATABASE || 'default',
+  // })
 
-// const query = await client.query({
-//   query: `SELECT
-//     toQuarter(Period) as quarter,
-//     toYear(Period) as year,
-//     Partner as partner,
-//     sum(Price) as value
-//   FROM Sales
-//   WHERE Period >= toDateTime(date_sub(now(), interval 4 year))
-//   GROUP BY partner, year, quarter
-//   ORDER BY partner, year, quarter;`,
-// })
+  // const query = await client.query({
+  //   query: `SELECT
+  //     toQuarter(Period) as quarter,
+  //     toYear(Period) as year,
+  //     Partner as partner,
+  //     sum(Price) as value
+  //   FROM Sales
+  //   WHERE Period >= toDateTime(date_sub(now(), interval 4 year))
+  //   GROUP BY partner, year, quarter
+  //   ORDER BY partner, year, quarter;`,
+  // })
 
-// const result = await query.json<{
-//   data: {
-//     partner: string
-//     year: number
-//     quarter: number
-//     value: number
-//   }[]
-// }>()
+  // const result = await query.json<{
+  //   data: {
+  //     partner: string
+  //     year: number
+  //     quarter: number
+  //     value: number
+  //   }[]
+  // }>()
 
-const result = {
-  data: Array.from({ length: 10000 }).map((_, i) => ({
-    partner: `Partner ${Math.floor(i / 10)}`,
-    year: Math.floor(Math.random() * 4) + 2019,
-    quarter: Math.floor(Math.random() * 4) + 1,
-    value: Math.floor(Math.random() * 10000) + 1000,
-  })),
-}
-
-// charts data
-const chartsData: IRow[][] = []
-
-// data is ordered by partner
-// so we can group it in one iteration
-let partner = ''
-for (const item of result.data) {
-  if (partner !== item.partner) {
-    partner = item.partner
-    chartsData.push([])
+  const result = {
+    data: Array.from({ length: 10000 }).map((_, i) => ({
+      partner: `Partner ${Math.floor(i / 100)}`,
+      year: Math.floor(Math.random() * 4) + 2019,
+      quarter: Math.floor(Math.random() * 4) + 1,
+      value: Math.floor(Math.random() * 10000) + 1000,
+    })),
   }
 
-  chartsData[chartsData.length - 1].push(item)
-}
+  // charts data
+  const chartsData: IRow[][] = []
 
-// and iterate through creating charts
-for (const data of chartsData) {
-  const chart = plot({
-    document,
+  // data is ordered by partner
+  // so we can group it in one iteration
+  let partner = ''
+  for (const item of result.data) {
+    if (partner !== item.partner) {
+      partner = item.partner
+      chartsData.push([])
+    }
 
-    width: 800,
-    height: 600,
+    chartsData[chartsData.length - 1].push(item)
+  }
 
-    color: {
-      scheme: 'tableau10',
-      legend: false,
-    },
+  // and iterate through creating charts
+  const promises = chartsData.map(async (data) => {
+    const chart = plot({
+      document,
+      width: 800,
+      height: 600,
+      color: {
+        scheme: 'tableau10',
+        legend: false,
+      },
+      x: {
+        label: 'год',
+        tickFormat: 'm',
+        paddingOuter: 0.2,
+      },
+      y: {
+        label: 'всего р.',
+        type: 'linear',
+        tickFormat: 's',
+        grid: true,
+      },
+      fx: {
+        label: 'квартал',
+      },
+      marks: [
+        barY(data, {
+          x: 'year',
+          y: 'value',
+          fill: 'year',
+          fx: 'quarter',
+        }),
+        ruleY([0]),
+      ],
+    })
 
-    x: {
-      label: 'год',
-      tickFormat: 'm',
-      paddingOuter: 0.2,
-    },
-
-    y: {
-      label: 'всего р.',
-      type: 'linear',
-      tickFormat: 's',
-      grid: true,
-    },
-
-    fx: {
-      label: 'квартал',
-    },
-
-    marks: [
-      barY(data, {
-        x: 'year',
-        y: 'value',
-        fill: 'year',
-        fx: 'quarter',
-      }),
-      ruleY([0]),
-    ],
+    return writeFile(`./out/${data[0].partner}.svg`, chart.outerHTML)
   })
 
-  chart.removeChild(chart.firstChild!)
-  writeFileSync(`./out/${data[0].partner}.svg`, chart.outerHTML)
-}
+  await Promise.all(promises)
+})()
